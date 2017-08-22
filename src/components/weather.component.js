e62bc4864ddcf405f033df13cf322dbf05b7e42c
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-08-22 15:14:18 -0500

    Added wind, dewpoint and humidity

--- src/components/weather.component.js
+++ src/components/weather.component.js
@@ -11,7 +11,12 @@ export default class WeatherComponent extends Component {
 				<h3>{this.props.weather.current_observation.display_location.full}</h3>
 				<h3>{this.props.weather.current_observation.weather}</h3>
 				<img src={this.props.weather.current_observation.icon_url} />
-				<h1>{this.props.weather.current_observation.temperature}</h1>
+				<h1>{this.props.weather.current_observation.temperature_string	}</h1>
+				<ul>
+					<li className="list-group-item"><strong>Relative Humidity:</strong>{this.props.weather.current_observation.relative_humidity}</li>
+					<li className="list-group-item"><strong>Wind:</strong>{this.props.weather.current_observation.wind_string}</li>
+					<li className="list-group-item"><strong>Dewpoint:</strong>{this.props.weather.current_observation.dewpoint_string}</li>
+				</ul>
 			</div>
 		);
 	}
